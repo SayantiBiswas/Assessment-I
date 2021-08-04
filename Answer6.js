f4d097fba03ddf06ee496c22cadb6125c76f914d
Sayanti Biswas <88278689+SayantiBiswas@users.noreply.github.com>
2021-08-04 20:31:27 +0530

    Create Answer6.js

---
+++ Answer6.js
@@ -0,0 +1,3 @@
+const set = new Set(['Beethoven', 'Mozart', 'Chopin', 'Chopin'])
+set.delete("Beethoven");//Delete item from set
+set.forEach((value) => console.log(value))//After delet item,print the result
